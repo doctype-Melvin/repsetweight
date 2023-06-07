2bf9b8ba95598053e2495cf95bf3e914010df99c
Melvin Speckamp <doctype.melvin@gmail.com>
2023-06-07 12:07:43 +0200

    add style.js for global styles

--- app/layout.js
+++ app/layout.js
@@ -2,13 +2,22 @@
 import MainLayout from "@/components/Layout";
 import React from "react";
 import StyledComponentsRegistry from "./registry";
-import "./global.css";
+import { GlobalStyle } from "./style.js";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <StyledComponentsRegistry>
+          <GlobalStyle />
           <MainLayout>{children}</MainLayout>
         </StyledComponentsRegistry>
       </body>
--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 "use client";
 
 import { styled } from "styled-components";
-import Form from "@/components/Form";
 
 export default function Home() {
   return (
