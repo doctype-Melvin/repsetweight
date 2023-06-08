3f2343e767343c45f1d379df1f1daa55ada82793
Melvin Speckamp <doctype.melvin@gmail.com>
2023-06-08 12:55:05 +0200

    add data model for exercises

---
+++ database/models/Exercises.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const exerciseSchema = new Schema({
+    name: String,
+    force: String,
+    equipment: String,
+    primaryMuscles: Array,
+    secondaryMuscles: Array,
+    category: String,
+})
+
+const Exercise = mongoose.models.Exercise || mongoose.model("Plan", exerciseSchema)
+
+export default Exercise;
