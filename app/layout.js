2b904b69100b55761ea4e5ed1d59b901e9152279
Melvin Speckamp <doctype.melvin@gmail.com>
2023-06-15 13:15:20 +0200

    remove unneccessary link components

--- app/layout.js
+++ app/layout.js
@@ -2,21 +2,10 @@ import MainLayout from "@/components/Layout";
 import React from "react";
 import "./global.css"
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body>     
           <MainLayout>{children}</MainLayout>
       </body>
--- app/page.js
+++ app/page.js
@@ -1,25 +1,15 @@
-"use client";
-
-import styled from "styled-components";
-import StyledLink from "@/components/StyledLink";
+import Link from "next/link";
+import "./styles.css"
 
 export default function Home() {
   return (
-    <PageContainer>
-      <StyledLink targetHref="/createTemplate" target="Create New Template" /> 
-      <StyledLink targetHref="/allTemplates" target="View All Templates" />
-    </PageContainer>
+    <section className="page__container">
+      <Link href="/createTemplate" className="link__styled"> New Template </Link>
+      <Link href="/allTemplates" className="link__styled"> All Templates </Link>
+    </section>
   );
 }
 
-const PageContainer = styled.section`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: repeat(auto-fill, 1fr);
-  justify-items: center;
-  row-gap: 1rem;
-  padding-top: 1rem;
-  padding-bottom: calc(20px + var(--navbar-height));
-`;
+
 
 
