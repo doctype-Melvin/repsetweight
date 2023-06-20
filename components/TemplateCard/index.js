2c682aaea54123b66fdfde3b5c25c76e2b8cdeb7
doctype-Melvin <doctype.melvin@gmail.com>
2023-06-20 08:40:17 +0200

    add styling to template card

--- components/TemplateCard/index.js
+++ components/TemplateCard/index.js
@@ -6,8 +6,8 @@ export default function TemplateCard( { template } ) {
         <li className="template__card">
         <Link className="template__link" href={`template/${template.id}`}>
         <div>{template.name}</div>
-        <div>{template.focus}</div>
         </Link>
+        <div className="template__focus">{template.focus}</div>
         <button type="button" className="edit__button">Edit</button>
         <button type="button" className="delete__button">Delete</button>
         </li>
