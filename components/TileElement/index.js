eeef5959ed0d68e9d8833a20cef07fcd7561774e
doctype-Melvin <doctype.melvin@gmail.com>
2023-07-14 13:35:45 +0200

    add tile rotation

--- components/TileElement/index.js
+++ components/TileElement/index.js
@@ -8,34 +8,36 @@ import gym from '../../public/gym.jpg'
 export default function TileElement(){
     return (
         <section className='container__tiles'>
-            <Image
-            src={barbell}
-            fill
-            alt='Image of a barbell with weight on the floor'
-            sizes='(max-width: 375px) 30vw'
-            className='tile__image barbell'
-            />
-           <Image
-            src={lifting}
-            fill
-            alt='Person with pony tail lifting barbell behind the head'
-            sizes='(max-width: 375px) 30vw'
-            className='tile__image lifting'
-            />
-            <Image
-            src={dumbbells}
-            fill
-            alt='Person with pony tail lifting barbell behind the head'
-            sizes='(max-width: 375px) 30vw'
-            className='tile__image dumbbells'
-            />
-            <Image
-            src={gym}
-            fill
-            alt='Person with pony tail lifting barbell behind the head'
-            sizes='(max-width: 375px) 30vw'
-            className='tile__image gym'
-            />
+            <div className='container__overflow'>
+                <Image
+                src={barbell}
+                fill
+                alt='Image of a barbell with weight on the floor'
+                sizes='(max-width: 375px) 30vw'
+                className='tile__image barbell'
+                />
+                           <Image
+                src={lifting}
+                fill
+                alt='Person with pony tail lifting barbell behind the head'
+                sizes='(max-width: 375px) 30vw'
+                className='tile__image lifting'
+                />
+                <Image
+                src={dumbbells}
+                fill
+                alt='Person with pony tail lifting barbell behind the head'
+                sizes='(max-width: 375px) 30vw'
+                className='tile__image dumbbells'
+                />
+                <Image
+                src={gym}
+                fill
+                alt='Person with pony tail lifting barbell behind the head'
+                sizes='(max-width: 375px) 30vw'
+                className='tile__image gym'
+                />
+            </div>
         </section>
     )
 }
