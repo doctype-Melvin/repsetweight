92310ed9b68c8a047f9861ab28e28dc0e985e8db
doctype-Melvin <doctype.melvin@gmail.com>
2023-07-14 12:38:29 +0200

    add grid element with image tiles

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import styles from "./styles.module.css";
+import TileElement from "@/components/TileElement";
 
 export default function Home() {
   return (
     <section className={styles.page__container}>
+      <TileElement />
       <Link href="/presets" className={styles.link__styled__span}>
         All Templates
       </Link>
