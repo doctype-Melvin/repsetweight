daab3555bb2599529f11891697d07666dc96a2db
doctype-Melvin <doctype.melvin@gmail.com>
2023-07-17 15:06:41 +0200

    prettier

--- components/ExerciseForm/index.js
+++ components/ExerciseForm/index.js
@@ -3,7 +3,7 @@
 import "./styles.css";
 import { useState } from "react";
 import ClientButton from "../ClientButton";
-import { AiOutlineSearch as Search } from "react-icons/ai"
+import { AiOutlineSearch as Search } from "react-icons/ai";
 
 export default function ExerciseForm({ exercises, handler }) {
   const [searchValue, setSearchValue] = useState("");
@@ -29,9 +29,13 @@ export default function ExerciseForm({ exercises, handler }) {
           onChange={handleChange}
           autoComplete="off"
         />
-        {searchValue === "" && <Search className="icon__search"/>}
+        {searchValue === "" && <Search className="icon__search" />}
         <div className="container__exercise-form__buttons">
-          <ClientButton textContent="Add" type="button" handler={() => onAdd(searchValue)} />
+          <ClientButton
+            textContent="Add"
+            type="button"
+            handler={() => onAdd(searchValue)}
+          />
           <ClientButton textContent="Close" type="button" handler={handler} />
         </div>
       </form>
--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import Link from "next/link";
-import { SlHome as Home } from "react-icons/sl"
+import { SlHome as Home } from "react-icons/sl";
 import { SlBookOpen as Book } from "react-icons/sl";
 import { AiOutlineFileAdd as Write } from "react-icons/ai";
 
@@ -8,7 +8,7 @@ export default function Navigation() {
   return (
     <nav className="navigation__container">
       <ul className="navigation__links">
-         <li key="home-link" className="navigation__item">
+        <li key="home-link" className="navigation__item">
           <Home />
           <Link className="navigation__link" href="/">
             Home
