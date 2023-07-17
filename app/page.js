41677b8e5cd40e15871978f4c0ebea0b40f2d7aa
doctype-Melvin <doctype.melvin@gmail.com>
2023-07-17 14:59:54 +0200

    add navigation styling and icons

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import styles from "./styles.module.css";
 import TileElement from "@/components/TileElement";
 import { SlBookOpen as Book } from "react-icons/sl";
-import { SlNote as Write } from "react-icons/sl";
+import { AiOutlineFileAdd as Write } from "react-icons/ai";
 import LinkCard from "@/components/LinkCard";
 
 export default function Home() {
--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,16 +1,27 @@
 import "./styles.css";
 import Link from "next/link";
+import { SlHome as Home } from "react-icons/sl"
+import { SlBookOpen as Book } from "react-icons/sl";
+import { AiOutlineFileAdd as Write } from "react-icons/ai";
 
 export default function Navigation() {
   return (
     <nav className="navigation__container">
       <ul className="navigation__links">
-        <li className="navigation__item">
+         <li key="home-link" className="navigation__item">
+          <Home />
+          <Link className="navigation__link" href="/">
+            Home
+          </Link>
+        </li>
+        <li key="all-templates-link" className="navigation__item">
+          <Book />
           <Link className="navigation__link" href="/presets">
             All Templates
           </Link>
         </li>
-        <li className="navigation__item">
+        <li key="create-template-link" className="navigation__item">
+          <Write />
           <Link className="navigation__link" href="/presets/new">
             Customize
           </Link>
