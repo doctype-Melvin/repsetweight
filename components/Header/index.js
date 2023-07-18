747a545d730424b9bb2950426ba22cd5703a5b59
doctype-Melvin <doctype.melvin@gmail.com>
2023-07-18 09:46:48 +0200

    add gradient to link cards

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,11 +1,9 @@
 import "./styles.css";
-import Navigation from "../Navigation";
 
 export default function Header() {
   return (
     <section className="header__container">
       <h1>GYM:log</h1>
-      {/* <Navigation /> */}
     </section>
   );
 }
