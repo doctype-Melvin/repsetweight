c66ecb9c79ad91977fa18e7911a91173603a3245
doctype-Melvin <doctype.melvin@gmail.com>
2023-07-25 15:41:24 +0200

    add search suggestions to exercise form

--- components/FormExerciseDetails/index.js
+++ components/FormExerciseDetails/index.js
@@ -2,14 +2,31 @@
 
 import "./styles.css"
 import ClientButton from "../ClientButton"
+import { useState } from "react"
 
 export default function FormExerciseDetails({exercises, handler}){
+    const [ searchValue, setSearchValue ] = useState("")
+    const [ name, setName ] = useState("")
+
+    const onAdd = (searchTerm) => {
+        setSearchValue(searchTerm);
+        setName(searchTerm);
+    }
+
+    const handleChange = (event) => {
+        setSearchValue(event.target.value);
+        setName("")
+    }
+
     return (
-        <form>
+        <section>
+        <form autoComplete="off">
                 <label htmlFor="exercise">
                     Exercise
                 </label>
-                    <input type="text" name="exercise" />
+                    <input type="text" name="exercise" value={searchValue} onChange={handleChange}
+                    />
+                    
                 <label htmlFor="sets">
                     Sets
                 </label>
@@ -23,9 +40,31 @@ export default function FormExerciseDetails({exercises, handler}){
                 </label>
                     <input type="number" name="weight" />
                 <div className="button__form">
-                <ClientButton type="submit" modifier="center add" textContent="Add" handler={handler} />
-                <ClientButton type="submit" modifier="center close" textContent="Close" handler={handler} />
+                <ClientButton type="button" modifier="center add" textContent="Add" handler={handler} />
+                <ClientButton type="button" modifier="center close" textContent="Close" handler={handler} />
                 </div>
             </form>
+            {name === "" && <div className="container__suggestions">
+                        {exercises.filter((exercise) => {
+                            const searchTerm = searchValue.toLowerCase();
+                            const exerciseName = exercise.name.toLowerCase();
+                            return (
+                                searchTerm &&
+                                exerciseName.includes(searchTerm) &&
+                                exerciseName !== searchTerm
+                            )
+                        }).slice(0, 5)
+                        .map((exercise) => (
+                            <div className="search__results"
+                            key={exercise.id}
+                            onClick={() => onAdd(exercise.name)}
+                            exercise={exercise}
+                            >
+                            {exercise.name}
+                            </div>
+                        ))
+                        }
+            </div>}
+        </section>
     )
 }
