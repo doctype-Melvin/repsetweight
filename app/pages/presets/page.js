dc7073aa54cc162928f9bcd54c464b5eee4db5bb
Melvin Speckamp <doctype.melvin@gmail.com>
2023-12-17 12:41:33 +0100

    add db connection attempt and mysql2

--- app/pages/presets/page.js
+++ app/pages/presets/page.js
@@ -3,10 +3,13 @@ import Link from "next/link";
 import TemplateCard from "@/components/TemplateCard";
 import dbConnect from "@/database/connectDB";
 import styles from "./styles.module.css";
+import { queryAllTemplates } from "@/database/connectDB";
 
 export default async function AllTemplates() {
   // await dbConnect();
   // const allPresets = await Preset.find();
-
+  const allTemplates = await queryAllTemplates({
+    query: "SELECT * FROM templates",
+  });
   return <section>List all presets here</section>;
 }
--- database/connectDB.js
+++ database/connectDB.js
@@ -1,10 +1,33 @@
 import mongoose from "mongoose";
+import mysql from "serverless-mysql";
+
+const db = mysql({
+  config: {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+  },
+});
+
+export async function queryAllTemplates({ query }) {
+  try {
+    const results = await db.query(query);
+    await db.end();
+    console.info("results", results);
+    return results;
+  } catch (error) {
+    console.error("error", error);
+    return { error };
+  }
+}
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local",
+    "Please define the MONGODB_URI environment variable inside .env.local"
   );
 }
 
