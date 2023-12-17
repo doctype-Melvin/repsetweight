2b03b95650f1beb94e3f82ef2c5c060685aea75b
Melvin Speckamp <doctype.melvin@gmail.com>
2023-12-17 10:50:43 +0100

    add responsive LinkCard grid

--- app/page.js
+++ app/page.js
@@ -1,10 +1,30 @@
+"use client";
+
 import LinkCard from "@/components/LinkCard";
+import { styled } from "styled-components";
+
+const LinkSection = styled.section`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto;
+  grid-column-gap: 1rem;
+  grid-row-gap: 1rem;
+  height: calc(100vh - var(--header-height));
+  padding: 1rem;
+
+  @media only screen and (min-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+  }
+`;
+
 export default function Home() {
   return (
-    <section>
+    <LinkSection>
       <LinkCard linkTitle="Create Workout Template" />
       <LinkCard linkTitle="Log Workout" />
       <LinkCard linkTitle="Browse Preset Templates" />
-    </section>
+      <LinkCard linkTitle="Workout History & Charts" />
+    </LinkSection>
   );
 }
--- components/LinkCard/index.js
+++ components/LinkCard/index.js
@@ -8,8 +8,13 @@ export default function LinkCard({ linkTitle, icon, description }) {
   return (
     <motion.div
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { duration: 1 } }}
-      whileHover={{ scale: 1.2, transition: { duration: 1 } }}
+      animate={{ opacity: 1, transition: { duration: 0.75 } }}
+      whileHover={{
+        scale: 1.05,
+        transition: { duration: 0.5 },
+        borderRadius: "5px",
+        backgroundColor: "var(--orange-light)",
+      }}
       className="link__motion"
     >
       <Link href="/link" className="link__styled">
