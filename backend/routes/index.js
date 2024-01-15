96de074aa5e8dd8a4978e5944c69fc73dcc034a7
Melvin Speckamp <doctype.melvin@gmail.com>
2024-01-15 12:36:58 +0100

    reset

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-const testController = require("../controllers/testController");
-
-/* GET home page. */
-router.get("/", testController.index);
-
-module.exports = router;
--- frontend/src/app/auth/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Authentication() {
-  return (
-    <section>
-      <h1>Authentication</h1>
-    </section>
-  );
-}
