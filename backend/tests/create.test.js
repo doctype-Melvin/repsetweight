53a39b3c32c084a43bd93e9b496757534d574279
Melvin Speckamp <doctype.melvin@gmail.com>
2024-02-19 13:49:41 +0100

    add mocked add template test ???

--- backend/tests/create.test.js
+++ backend/tests/create.test.js
@@ -2,7 +2,7 @@ const supertest = require("supertest");
 const app = require("../app");
 const session = require("supertest-session");
 const { models } = require("../database/dbConnect");
-const { Exercise } = models;
+const { Exercise, Template } = models;
 require("dotenv").config();
 
 jest.mock("../database/dbConnect", () => ({
@@ -40,5 +40,20 @@ describe("CREATE functionality for different routes", () => {
 
     const response = await api.post("/api/exercises/add").send(mockExercise);
     expect(response.body).toEqual({ data: mockExercise });
+  });
+
+  it("should create a template", async () => {
+    const mockTemplate = {
+      name: "test",
+      description: "test",
+      user_added: 1,
+    };
+
+    const response = await api.post("/api/templates/add").send(mockTemplate);
+    expect(response.status).toBe(200);
+  });
+
+  afterAll(async () => {
+    api.destroy();
   });
 });
