817bbde57b562121cf282d31a1fe0c5635467563
Melvin Speckamp <doctype.melvin@gmail.com>
2024-03-19 09:09:06 +0100

    add exercise detail view

--- frontend/src/routes/exercises/+page.svelte
+++ frontend/src/routes/exercises/+page.svelte
@@ -5,7 +5,7 @@
 <h1>All Exercises</h1>
 <ul>
     {#each data.result.data as exercise}
-        <li><a href="/exercises/{exercise.id}">{exercise.name}</a></li>
+        <li><a href="/exercises/{exercise.exercise_id}">{exercise.name}</a></li>
     {/each}
 </ul>
 <a href="/">Dashboard</a>
---
+++ frontend/src/routes/exercises/[slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let data
+</script>
+
+<h1>{data.result.data.name}</h1>
+
+<ul>
+        <li>Muscle: {data.result.data.target}</li>
+        <li>Category: {data.result.data.category}</li>
+        <li>Equipment: {data.result.data.equipment}</li>
+    
+</ul>
+<a href="/exercises">Exercises</a>
