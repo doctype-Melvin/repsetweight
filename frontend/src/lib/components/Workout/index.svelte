35af6ec3448117b06b8865146e2716114599b9df
Melvin Speckamp <doctype.melvin@gmail.com>
2024-04-05 10:10:09 +0200

    add few comments for further development

--- frontend/src/lib/components/Workout/index.svelte
+++ frontend/src/lib/components/Workout/index.svelte
@@ -2,12 +2,17 @@
     // @ts-nocheck
     export let data;
 </script>
-
+<!-- Workout Component -->
 
 <h3>{data.name}</h3>
     <ul class="exercise-list">
         {#each data.Exercises as exercise}
             <li>
+                <!--
+                    Exercise component needed
+                    Features: change, delete exercise thorugh
+                    pop up menu
+                 -->
                 <h4>{exercise.name}</h4>
                 
             </li>
