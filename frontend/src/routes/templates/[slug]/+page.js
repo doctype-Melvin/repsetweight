9df3a05cf591c86aa598146cc3dca72e7132f8ba
Melvin Speckamp <doctype.melvin@gmail.com>
2024-04-17 10:51:25 +0200

    add comment to template detail load fn

--- frontend/src/routes/templates/[slug]/+page.js
+++ frontend/src/routes/templates/[slug]/+page.js
@@ -1,4 +1,8 @@
 export async function load({ fetch, params }) {
+	// Exercises are initially fetched but
+	// got lost on page reload
+	// Thus placing another fetch in the route
+	// Alternative: store data in local storage
 	const responseExercises = await fetch('http://localhost:3000/api/exercises/all');
 	const responseTemplate = await fetch(`http://localhost:3000/api/templates/detail/${params.slug}`);
 
