d58a83ad69984b51caceafb70d92523ea498d300
Melvin Speckamp <doctype.melvin@gmail.com>
2024-07-09 12:07:00 +0200

    add test cases for Composer

--- frontend/src/lib/components/Composer/Composer.test.js
+++ frontend/src/lib/components/Composer/Composer.test.js
@@ -1,29 +1,49 @@
-import { render, screen } from '@testing-library/svelte';
+//@ts-nocheck
+
+import { render, screen, fireEvent } from '@testing-library/svelte';
 import { beforeEach, expect, test, vi } from 'vitest';
 import * as stores from '../../stores.js';
 
 import Composer from './Composer.svelte';
 
 // Composer imports isWriteMode directly from stores module
-// By default isWriteMode is false
+// By default isWriteMode is false but
+// Composer lives in the create route
+// whenever the create route is visited
+// isWriteMode is set to true
 
 // Mock the entire stores module
-vi.mock('@lib/stores.js', () => {
+
+vi.mock('@lib/stores.js', async () => {
     return {
         isWriteMode: {
             // Mock the subscribe method
             subscribe: vi.fn((cb) => {
-                // Set store value to true
-                cb(true)
+                // Set store value to false
+                // to simulate the default value
+                cb(false)
                 // Unsubscribe function
                 return () => ({})
             })
+        },
+        userTemplateData: {
+            subscribe: vi.fn((cb) => {
+                cb([])
+                return () => ({})
+            }),
+            set: vi.fn((data) => console.log(data))
         }
     }
  })
 
 beforeEach(() => {
-    vi.resetAllMocks()
+    // Since the create route always sets isWriteMode to true
+    // We mock the subscribe method to set the store value to true
+    vi.spyOn(stores.isWriteMode, 'subscribe').mockImplementation((cb) => {
+        cb(true)
+        return () => ({})
+    })
+
 })
 
 
@@ -34,41 +54,28 @@ test('Composer renders and shows default message', () => {
     expect(composerElement).toBeInTheDocument();
 })
 
-test('Composer does not render add muscle group button when isWriteMode is false', () => {
-    render(Composer)
-    const addMuscleGroupButton = screen.queryByRole("button", {name: 'Add Muscle Group'})
-
-    expect(addMuscleGroupButton).not.toBeInTheDocument();
-})
-
-test('Composer does not render add workout button when isWriteMode is false', () => {
-    render(Composer)
-    const addWorkoutButton = screen.queryByRole("button", {name: 'Add Workout'})
-
-    expect(addWorkoutButton).not.toBeInTheDocument();
-})
-
-test('Composer renders add muscle group button when isWriteMode is true', () => {
-    vi.spyOn(stores.isWriteMode, 'subscribe').mockImplementation((cb) => {
-        cb(true)
-        return () => ({})
-    })
-
-    render(Composer)
-    const addMuscleGroupButton = screen.getByRole("button", {name: 'Add Muscle Group'}) 
-
-    expect(addMuscleGroupButton).toBeInTheDocument();
-})
 
 test('Composer has add workout button when isWriteMode is true', () => {
-    
-    vi.spyOn(stores.isWriteMode, 'subscribe').mockImplementation((cb) => {
-        cb(true)
-        return () => ({})
-    })
-
     render(Composer)
     const addWorkoutButton = screen.getByRole("button", {name: 'Add Workout'})
     
     expect(addWorkoutButton).toBeInTheDocument();
 })
+
+test('Composer renders a single empty workout component for a new template', () => {
+    render(Composer)
+    const workoutComponent = screen.getAllByRole('listitem')
+
+    expect(workoutComponent).toHaveLength(1);
+})
+
+test('Composer adds a workout when Add Workout button is clicked', async () => {
+    render(Composer)
+    const addWorkoutButton = screen.getByRole("button", {name: 'Add Workout'})
+    await fireEvent.click(addWorkoutButton)
+
+    const workoutComponent = screen.getAllByRole('listitem')
+
+    expect(workoutComponent).toHaveLength(2);
+})
+
