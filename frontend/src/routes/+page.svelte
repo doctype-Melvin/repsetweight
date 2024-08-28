024ac9a02c3ea69e624901a08ecd688d75d6690a
Melvin Speckamp <doctype.melvin@gmail.com>
2024-08-28 10:13:03 +0200

    add sidebar

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,12 +2,13 @@
 	// @ts-nocheck
 	import { goto } from '$app/navigation';
 	import { templatesData, isWriteMode } from '$lib/stores.js';
+	import Sidebar from '$lib/components/Sidebar/Sidebar.svelte';
 
 	isWriteMode.set(false);
 </script>
 
-<button type="button" on:click={() => goto('/create')}>Create Template</button>
-<button type="button" on:click={() => goto('/templates')}>All Templates</button>
+<Sidebar />
+
 {#if !$isWriteMode}
 	<p><b>Read only mode</b></p>
 {/if}
